f221946ba21b0100903275e721bcfbd62c46ecfc
JCBolivar <jcbolivargarcia83@gmail.com>
2021-11-25 11:19:01 +0100

    Genero class Turno, cliente y tienda

--- src/pescaderia/Tienda.java
+++ src/pescaderia/Tienda.java
@@ -1,5 +1,22 @@
 package pescaderia;
 
+import java.util.List;
+
 public class Tienda {
+	private Turno turnoActual;
+	private Turno ultimoTurnoDado;
+	private List<Cliente> clientes;
+
+	public Turno getTurnoActual() {
+		return turnoActual;
+	}
+
+	public Turno darTurno(Cliente cliente) {
+		Turno turno = new Turno(ultimoTurnoDado.getValor()+1);
+		return null;
+		
+	}
+	
+	
 
 }
